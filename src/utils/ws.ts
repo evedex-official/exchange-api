f6f55bbcc6daf9356d218f0f6e228ec557004e7c
Dmitry Vdovichenko <dmitry.vdovichencko@gmail.com>
2026-01-30 06:57:18 +0000

    fix: extends subscription options with centrifuge types

--- src/utils/ws.ts
+++ src/utils/ws.ts
@@ -7,9 +7,42 @@ export interface CentrifugeSubscription {
 
   unsubscribe(): void;
 }
+export interface SubscriptionTokenContext {
+  channel: string;
+}
+export interface SubscriptionDataContext {
+  channel: string;
+}
 
-export interface AssignChannelOptions {
+/** Stream postion describes position of publication inside a stream.  */
+export interface StreamPosition {
+  offset: number;
+  epoch: string;
+}
+
+export interface SubscriptionOptions {
+  /** allows setting initial subscription token (JWT) */
+  token: string;
+  /** allows setting function to get/refresh subscription token */
+  getToken: null | ((ctx: SubscriptionTokenContext) => Promise<string>);
+  /** data to send to a server with subscribe command */
+  data: any | null;
+  /** allows setting function to get/renew subscription data */
+  getData: null | ((ctx: SubscriptionDataContext) => Promise<any>);
+  /** force recovery on first subscribe from a provided StreamPosition. */
+  since: StreamPosition | null;
+  /** min delay between resubscribe attempts. */
+  minResubscribeDelay: number;
+  /** max delay between resubscribe attempts. */
+  maxResubscribeDelay: number;
+  /** ask server to make subscription positioned. */
+  positioned: boolean;
+  /** ask server to make subscription recoverable. */
   recoverable: boolean;
+  /** ask server to send join/leave messages. */
+  joinLeave: boolean;
+  /** delta format to be used */
+  delta: "fossil";
 }
 
 export interface CentrifugeClient {
@@ -19,5 +52,5 @@ export interface CentrifugeClient {
 
   connect(): void;
 
-  assignChannel(name: string, options?: AssignChannelOptions): CentrifugeSubscription;
+  assignChannel(name: string, options?: Partial<SubscriptionOptions>): CentrifugeSubscription;
 }
